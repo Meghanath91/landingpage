b694e7ebc5cf7af78494e615bef608030104c2ea
Meghanath91 <meghanath.balaji@gmail.com>
2021-03-26 10:35:08 -0400

    add navbar structure

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import { CssBaseline } from '@material-ui/core'
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function MainPage() {
+  return (
+    <div>
+      <CssBaseline />
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import { AppBar } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import logo from "../images/logo.svg";
+import registerBtn from "../images/reigster-button-lg.jpg";
+
+export default function Navbar() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    },
+    appBar: {
+      background: "none",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    registerBtn: {},
+    registerBtnImg: {
+      height: "80px",
+    },
+    logoImg: {
+      height: "80px",
+      padding: "25px"
+    },
+  }));
+
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar className={classes.appBar}>
+        {/* <div>
+
+        </div> */}
+        <img className={classes.logoImg} src={logo} alt="logo" />
+        <img
+          className={classes.registerBtnImg}
+          src={registerBtn}
+          alt="register-button"
+        />
+      </AppBar>
+    </div>
+  );
+}
